67da48d187925ba6e033363f2204ee5e1bdb1c1e
shubham-mittal-2006 <shubhammittal29032006@gmail.com>
2026-04-21 01:16:47 +0530

    force latest forms deploy

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,19 +1,24 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "..", "uploads");
+
+// folder nahi hai to create karo
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadPath);
   },
 
   filename: function (req, file, cb) {
     const unique =
       Date.now() + "-" + Math.round(Math.random() * 1e9);
 
-    cb(
-      null,
-      unique + path.extname(file.originalname)
-    );
+    cb(null, unique + path.extname(file.originalname));
   }
 });
 
